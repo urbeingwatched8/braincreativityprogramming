7bb4952ca8b4666cfbbcd661c4b029c4aaffb1a2
Nataliya Tupikina <35970083+urbeingwatched8@users.noreply.github.com>
2022-06-28 12:16:06 +0300

    Update opennumpy.py

--- opennumpy.py
+++ opennumpy.py
@@ -9,3 +9,21 @@ for i in range(len(data)):
 avg=sum1/len(data)
 print(avg)
 
+#for comparing first 10 epochs per file to last 10 epochs
+#import numpy as np
+#data = np.load('D:\\experiments\\artemBnpy\\artempaint1alpha.npy')
+#print(len(data))
+#print(len(data[0]))
+#print(data[0])
+#sum1=0
+#for i in range(10):
+#    sum1=sum1+sum(data[i])/len(data[i])
+#avg=sum1/10
+#print(avg)
+
+#sum2=0
+#for j in range(len(data)-10,len(data)):
+#    sum2=sum2+sum(data[j])/len(data[j])
+#avg2=sum2/10
+#print(avg2)
+
